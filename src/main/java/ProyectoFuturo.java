3c1f0cce2da9aa1bc5ffafceec89eae5dbb50cdf
Andres <andres10penalva@gmail.com>
2022-11-21 21:10:13 +0100

    private por public

---
+++ src/main/java/ProyectoFuturo.java
@@ -0,0 +1,18 @@
+public class ProyectoFuturo {
+    public static void main(String[]args){
+        System.out.println("ESTUDIANTE "+args[0]+ " "+ args[1]);
+        System.out.println(veredicto (args[3], args[2]));
+        System.out.println("Mi nombres es Andres");
+    }
+    
+    private static String veredicto (String valor1, String valor2){
+    if (valor1.equals(valor2)) {
+    return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+}
+if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+    return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+}
+return "Ideal. Trabajas los contenidos en casa.";
+    }
+    
+}
